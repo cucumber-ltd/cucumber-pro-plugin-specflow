86921ca2fa57a85f9e2fc76adece6d4ebf71de53
Gaspar Nagy <gaspar.nagy@gmail.com>
2018-02-05 15:44:02 +0100

    added empty project for the plugin

---
+++ src/Cucumber.Pro.SpecFlowPlugin/Cucumber.Pro.SpecFlowPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cucumber.Pro.SpecFlowPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cucumber Ltd")]
+[assembly: AssemblyProduct("Cucumber.Pro.SpecFlowPlugin")]
+[assembly: AssemblyCopyright("Copyright © Cucumber Ltd 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("073873ef-7722-4e78-acab-2f5d27a73f90")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
