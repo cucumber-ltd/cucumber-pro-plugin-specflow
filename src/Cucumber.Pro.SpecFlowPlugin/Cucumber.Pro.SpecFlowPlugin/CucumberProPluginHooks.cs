5275122788eb00d3ba64e5c55fb0f05d378898c2
Gaspar Nagy <gaspar.nagy@gmail.com>
2018-02-05 17:12:56 +0100

    add dummy tracing

--- src/Cucumber.Pro.SpecFlowPlugin/Cucumber.Pro.SpecFlowPlugin/CucumberProPluginHooks.cs
+++ src/Cucumber.Pro.SpecFlowPlugin/Cucumber.Pro.SpecFlowPlugin/CucumberProPluginHooks.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Reflection;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Tracing;
 
 namespace Cucumber.Pro.SpecFlowPlugin
 {
@@ -9,11 +10,18 @@ namespace Cucumber.Pro.SpecFlowPlugin
     public class CucumberProPluginHooks
     {
         private static int _counter = 0;
+        private readonly ITraceListener _traceListener;
+
+        public CucumberProPluginHooks(ITraceListener traceListener)
+        {
+            _traceListener = traceListener;
+        }
 
         [BeforeScenario]
         public void Test1()
         {
             _counter++;
+            _traceListener.WriteToolOutput($"starting scenario #{_counter}");
         }
 
         [AfterTestRun]
