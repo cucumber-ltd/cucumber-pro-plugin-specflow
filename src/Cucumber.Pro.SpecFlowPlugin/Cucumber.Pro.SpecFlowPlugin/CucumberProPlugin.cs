ecf4f99cc6ae2e9bfabeb2d400621be9afb2a97b
Gaspar Nagy <gaspar.nagy@gmail.com>
2018-02-05 15:59:56 +0100

    setup empty plugin

---
+++ src/Cucumber.Pro.SpecFlowPlugin/Cucumber.Pro.SpecFlowPlugin/CucumberProPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cucumber.Pro.SpecFlowPlugin;
+using TechTalk.SpecFlow.Plugins;
+
+[assembly: RuntimePlugin(typeof(CucumberProPlugin))]
+
+namespace Cucumber.Pro.SpecFlowPlugin
+{
+    public class CucumberProPlugin : IRuntimePlugin
+    {
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters)
+        {
+        }
+    }
+}
