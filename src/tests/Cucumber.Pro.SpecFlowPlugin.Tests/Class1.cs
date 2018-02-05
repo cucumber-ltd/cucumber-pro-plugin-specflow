cdfcbea07a3b1e35ddefc70482be2f9eac309171
Gaspar Nagy <gaspar.nagy@gmail.com>
2018-02-05 16:44:00 +0100

    add empty tests project

---
+++ src/tests/Cucumber.Pro.SpecFlowPlugin.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cucumber.Pro.SpecFlowPlugin.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
