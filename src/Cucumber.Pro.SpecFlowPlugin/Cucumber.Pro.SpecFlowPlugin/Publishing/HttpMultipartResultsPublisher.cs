05221479d550b117c11687df5456e36b31a0fbf5
Gaspar Nagy <gaspar.nagy@gmail.com>
2018-02-08 15:52:58 +0100

    cleanup publish happy path

--- src/Cucumber.Pro.SpecFlowPlugin/Cucumber.Pro.SpecFlowPlugin/Publishing/HttpMultipartResultsPublisher.cs
+++ src/Cucumber.Pro.SpecFlowPlugin/Cucumber.Pro.SpecFlowPlugin/Publishing/HttpMultipartResultsPublisher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -9,26 +10,55 @@ namespace Cucumber.Pro.SpecFlowPlugin.Publishing
 {
     public class HttpMultipartResultsPublisher : IResultsPublisher
     {
+        private const string PART_ENV = "env";
+        private const string PART_PAYLOAD = "payload";
+        private const string PART_PROFILE_NAME = "profileName";
+        private const string CONTENT_TYPE_CUCUMBER_JAVA_RESULTS_JSON = "application/x.cucumber.java.results+json";
+        private const string CONTENT_TYPE_SPECFLOW_RESULTS_JSON = CONTENT_TYPE_CUCUMBER_JAVA_RESULTS_JSON; // "application/x.specflow.results+json";
+
+        private readonly string _url;
+
+        public HttpMultipartResultsPublisher(string url = "https://app.cucumber.pro/tests/results/SpecSol_Test1/master")
+        {
+            _url = url;
+        }
+
         public void PublishResults(string resultsJsonFilePath, IDictionary<string, string> env, string profileName)
         {
             var resultsJson = File.ReadAllText(resultsJsonFilePath);
+            PublishResultsFromContent(env, profileName, resultsJson);
+        }
 
+        private void PublishResultsFromContent(IDictionary<string, string> env, string profileName, string resultsJson)
+        {
             var httpClient = new HttpClient();
-            //httpClient.BaseAddress = new Uri("https://app.cucumber.pro/tests/results/SpecSol_Test1");
 
-            httpClient.DefaultRequestHeaders.Authorization =
-                new AuthenticationHeaderValue("Basic",
-                    Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes("fe3e1a5f27789a139a963ff56cddb00816c" + ":")));
+            SetupAuthorization(httpClient);
+
             var content = new MultipartFormDataContent();
-            content.Add(new StringContent(@"cucumber_pro_build_reference=build123
-cucumber_pro_git_branch=master
-".Replace("\r\n", "\n")), "env", "env.txt");
-            content.Add(new StringContent("default"), profileName);
-            content.Add(new StringContent(resultsJson, Encoding.UTF8,
-                "application/x.cucumber.java.results+json"), "payload", "results.json");
-            var response = httpClient
-                .PostAsync("https://app.cucumber.pro/tests/results/SpecSol_Test1/master", content).Result;
+            content.Add(new StringContent(profileName), PART_PROFILE_NAME);
+            content.Add(GetEnvContent(env), PART_ENV, "env.txt");
+            content.Add(GetPayloadContent(resultsJson), PART_PAYLOAD, "results.json");
+
+            var response = httpClient.PostAsync(_url, content).Result;
             Console.WriteLine(response);
+        }
+
+        private static StringContent GetPayloadContent(string resultsJson)
+        {
+            return new StringContent(resultsJson, Encoding.UTF8, CONTENT_TYPE_SPECFLOW_RESULTS_JSON);
+        }
+
+        private static StringContent GetEnvContent(IDictionary<string, string> env)
+        {
+            return new StringContent(string.Join("\n", env.Select(e => $"{e.Key}={e.Value}")));
+        }
+
+        private void SetupAuthorization(HttpClient httpClient)
+        {
+            var token = "fe3e1a5f27789a139a963ff56cddb00816c";
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes(token + ":")));
         }
     }
 }
---
+++ src/tests/Cucumber.Pro.SpecFlowPlugin.Tests/Publishing/HttpMultipartResultsPublisherTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Cucumber.Pro.SpecFlowPlugin.Publishing;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Hosting.Self;
+using Xunit;
+
+namespace Cucumber.Pro.SpecFlowPlugin.Tests.Publishing
+{
+    public class HttpMultipartResultsPublisherTests
+    {
+        public class CProStubNancyModule : NancyModule
+        {
+            public static bool IsInvoked;
+            public static string ProjectName;
+            public static string Revision;
+            public static string ProfileName;
+            public static string Json;
+            public static Dictionary<string, string> Env;
+
+            public CProStubNancyModule()
+            {
+                Post["{projectName}/{revision}"] = parameters =>
+                {
+                    IsInvoked = true;
+
+                    ProjectName = parameters.projectName;
+                    Revision = parameters.revision;
+
+                    ProfileName = Request.Form["profileName"];
+
+                    var envStream = Request.Files.First(f => f.Key.Contains("env")).Value;
+                    var envContent = new StreamReader(envStream).ReadToEnd();
+                    Env = envContent.Split('\n').Where(l => !string.IsNullOrWhiteSpace(l)).ToDictionary(l => l.Split(new[] { '=' }, 2).First(), l => l.Split(new[] { '=' }, 2).Last());
+
+                    var jsonStream = Request.Files.First(f => f.Key.Contains("payload")).Value;
+                    Json = new StreamReader(jsonStream).ReadToEnd();
+
+                    return 200;
+                };
+            }
+        }
+
+        [Fact]
+        public void posts_results_as_multipart_formadata()
+        {
+            var tempFile = Path.GetTempFileName();
+            var json = @"[{ ""name"": ""Eating cucumbers""}]";
+            File.WriteAllText(tempFile, json);
+            var env = new Dictionary<string, string> { { "env1", "value1" }, { "env2", "value2" } };
+            var profileName = "my-profile";
+
+            var hostConfiguration = new HostConfiguration {RewriteLocalhost = false};
+            using (var nancyHost = new NancyHost(new Uri("http://localhost:8082/tests/results/"), NancyBootstrapperLocator.Bootstrapper, hostConfiguration))
+            {
+                nancyHost.Start();
+
+                var publisher = new HttpMultipartResultsPublisher("http://localhost:8082/tests/results/prj/rev1");
+                publisher.PublishResults(tempFile, env, profileName);
+            }
+
+            Assert.True(CProStubNancyModule.IsInvoked);
+            Assert.Equal("prj", CProStubNancyModule.ProjectName);
+            Assert.Equal("rev1", CProStubNancyModule.Revision);
+            Assert.Equal(profileName, CProStubNancyModule.ProfileName);
+            Assert.Equal(env, CProStubNancyModule.Env);
+            Assert.Equal(json, CProStubNancyModule.Json);
+        }
+    }
+}
